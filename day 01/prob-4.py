50032f423c5fcdc5632dd144fe421f50bfbe8cb9
fahim-nion <fahimalhassan4@gmail.com>
2025-11-09 17:23:30 +0600

    exploring day 1

---
+++ day 01/prob-4.py
@@ -0,0 +1,24 @@
+'''
+Practice Problem - 4: Create variables, identify and print the data type of each. ( At least 4 datatypes ) 
+'''
+
+data1 = 10
+data2 = True
+data3 = 12.2
+data4 = 96j
+data5 = [1,2,4]
+
+print(type(data1))
+print(type(data2))
+print(type(data3))
+print(type(data4))
+print(type(data5))
+
+'''
+Output:
+<class 'int'>
+<class 'bool'>
+<class 'float'>
+<class 'complex'>
+<class 'list'>
+'''
