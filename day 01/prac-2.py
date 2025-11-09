06ffd7b1532a898543da7a99251286f05ba4f3ff
fahim-nion <fahimalhassan4@gmail.com>
2025-11-09 16:51:37 +0600

    exploring day 1

---
+++ day 01/prac-2.py
@@ -0,0 +1,10 @@
+'''
+Practice Problem - 2: Simple Calculator ( take two input from user and just do simple calculation like addition) 
+'''
+
+val1 = int(input("Enter first value: "))
+val2 = int(input("Enter second value: "))
+
+sum = val1+val2
+
+print(f"Your sum is : {sum}")
