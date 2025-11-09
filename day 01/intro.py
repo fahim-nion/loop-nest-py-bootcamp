6beb1ab4a53ca70c86c06ba763de2f5721740f87
fahim-nion <fahimalhassan4@gmail.com>
2025-11-09 15:41:30 +0600

    exploring day 1

--- day 01/intro.py
+++ day 01/intro.py
@@ -10,3 +10,24 @@ comment
 
 #Variables-->
 #variable is a name that stores different kinds of data
+
+var = 10
+print(var)
+
+
+#casting
+x = int(10)
+y = float(9.00001)
+
+print(y)
+
+'''
+var must start wirth a letter and _ char
+
+'''
+
+a,b,c = 10,20,30.5
+d = e = f = 10
+
+reason = input("Why are you here?")
+print(reason)
