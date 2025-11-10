341f0f077eabd086658be38db3282b447b1d3316
fahim-nion <fahimalhassan4@gmail.com>
2025-11-10 14:38:48 +0600

    exploring day 2

---
+++ day 02/prob-8.py
@@ -0,0 +1,9 @@
+'''Problem 8: 
+You have a string that contains a bad word, and you want to censor it using *. Replace the word with "***".
+'''
+
+speech = "You dumbass! How could you do this to me!"
+
+updated_speech = speech.replace("dumbass","*******")
+
+print(updated_speech)
