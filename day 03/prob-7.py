2cc2d7b73e95b1b0b0a37872bed5bdd6e1a175d5
fahim-nion <fahimalhassan4@gmail.com>
2025-11-11 16:08:09 +0600

    exploring day 3

---
+++ day 03/prob-7.py
@@ -0,0 +1,19 @@
+'''
+Practice Problem-7: Access and Update Dictionary Value
+You are given the following dictionary:
+student = {"name": "Ayaan", "age": 17, "grade": "A"}
+
+"""
+Task:
+   - Access the value of "grade" and print it.
+   - Then, update "age" to 18.
+"""
+
+'''
+student = {
+    "name": "Ayaan", 
+    "age": 17, 
+    "grade": "A"
+    }
+
+print(student["grade"])
