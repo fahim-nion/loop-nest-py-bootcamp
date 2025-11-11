584be1e9072fc02f4b8c2a98ecbf0c78ff97c398
fahim-nion <fahimalhassan4@gmail.com>
2025-11-11 16:02:21 +0600

    exploring day 3

---
+++ day 03/prob-6.py
@@ -0,0 +1,10 @@
+'''
+Practice Problem-6: 
+
+Count an Item in a Tuple
+You are given a tuple:
+marks = (90, 85, 90, 75, 90, 80)
+#Use a tuple method to find out how many times 90 appears in the tuple.
+'''
+
+marks = (90, 85, 90, 75, 90, 80)
