f097222883d7c91d39ccc256859a1ddb81939c93
fahim-nion <fahimalhassan4@gmail.com>
2025-11-11 15:42:41 +0600

    exploring day 3

---
+++ day 03/prob-4.py
@@ -0,0 +1,15 @@
+'''
+Practice Problem-4: 
+Find Common Hobbies
+
+You are given two sets of hobbies from two friends:
+friend_1 = {"reading", "traveling", "gaming"}
+friend_2 = {"photography", "traveling", "gaming"}
+
+'''
+friend_1 = {"reading", "traveling", "gaming"}
+friend_2 = {"photography", "traveling", "gaming"}
+
+frnd1 = list(friend_1)
+frnd2 = list(friend_2)
+
