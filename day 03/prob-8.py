5a53eea8f608dfccebf4e5a5974082b8cae5bb2a
fahim-nion <fahimalhassan4@gmail.com>
2025-11-11 16:13:02 +0600

    exploring day 3

---
+++ day 03/prob-8.py
@@ -0,0 +1,13 @@
+'''
+Practice Problem-8: Add a New Key-Value Pair
+You are given an empty dictionary:
+profile = {}
+
+"""
+Task:
+-Add "username" with the value "tech_wizard".
+-Add "followers" with the value 1500.
+-Print the updated dictionary. 
+"""
+
+'''
