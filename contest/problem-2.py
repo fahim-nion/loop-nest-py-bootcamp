ecfb41ee25d23db595d9da9b6dc240a08fb28483
fahim-nion <fahimalhassan4@gmail.com>
2025-11-12 23:08:56 +0600

    bootcamp contest

---
+++ contest/problem-2.py
@@ -0,0 +1,15 @@
+'''
+2. Strings & String Methods
+
+● Problem: "Title Case Converter"
+
+○ Description: Given a string of words separated by spaces (e.g., "hello world
+in python"), use a string method to convert the string so that the first letter of
+every word is capitalized and the rest are lowercase. Print the resulting string.
+○ Input Example: "wELl, thAt's an interesting stRING"
+○ Expected Output: "Well, That's An Interesting String"
+'''
+
+str_inp = input("Enter String: ")
+
+print(str_inp.title())
