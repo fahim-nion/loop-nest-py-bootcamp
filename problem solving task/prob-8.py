50343af2c50eca84f5ffaa13114c87e11a873326
fahim-nion <fahimalhassan4@gmail.com>
2025-11-14 23:42:15 +0600

    problem solving

---
+++ problem solving task/prob-8.py
@@ -0,0 +1,19 @@
+'''
+1. Create a dictionary for a student with keys: `'name'`, `'id'`, and `'dept'`. Print only the department from the dictionary.
+
+**Example Input:**
+
+`{'name': 'Rup', 'id': 102, 'dept': 'CSE'}`
+
+**Expected Output:**
+
+`CSE`
+'''
+
+std_dict = {
+    "name" : "Abdul",
+    "id" : 1234566,
+    "dept": "CSE"
+}
+
+print(std_dict["dept"])
