1a1983f075c2fc5bd75d82f3f81525eaed35fad9
fahim-nion <fahimalhassan4@gmail.com>
2025-11-14 23:45:15 +0600

    exploring day 4

---
+++ problem solving task/prob-10.py
@@ -0,0 +1,20 @@
+'''
+1. You have two sets:
+
+`set1 = {1, 2, 3, 4}`
+
+`set2 = {3, 4, 5, 6}`
+
+Find the **common elements** and print them.
+
+**Expected Output:**
+
+`{3, 4}`
+'''
+
+set1 = {1, 2, 3, 4}
+
+set2 = {3, 4, 5, 6}
+
+
+print(set1 & set2)
