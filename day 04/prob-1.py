f2aa6ab766c4bdcd58116f84f0fec4bafbef2c6a
fahim-nion <fahimalhassan4@gmail.com>
2025-11-14 22:55:41 +0600

    exploring day 4

---
+++ day 04/prob-1.py
@@ -0,0 +1,15 @@
+'''
+🔁 Problem 1: Count Down from 5
+📝 Task:
+Use a while loop to print numbers from 5 to 1 in descending order.
+🎯 Example Output:
+5
+4
+3
+2
+1
+
+'''
+
+for i in range(5,0,-1):
+    print(i)
