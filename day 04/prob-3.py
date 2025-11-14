2c9c855782871e91c95278ef8769580c5cceb8a5
fahim-nion <fahimalhassan4@gmail.com>
2025-11-14 23:16:29 +0600

    exploring day 4

---
+++ day 04/prob-3.py
@@ -0,0 +1,16 @@
+'''
+### 🔁 **Problem 3: Sum of First 10 Numbers**
+
+**📝 Task:**
+
+Use a `for` loop to calculate the **sum of numbers from 1 to 10** and print the result.
+
+**🎯 Example Output:**
+
+'''
+
+
+sum = 0
+for i in range(11):
+    sum+=i
+print(sum)
