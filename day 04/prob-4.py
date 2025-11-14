b25054a2b90db4d0f18d3a674c4cb98702ded481
fahim-nion <fahimalhassan4@gmail.com>
2025-11-14 23:17:49 +0600

    exploring day 4

---
+++ day 04/prob-4.py
@@ -0,0 +1,17 @@
+'''
+🔄 Problem 4: Print Each Character of a String
+📝 Task:
+Take a string (e.g. "Python") and use a for loop to print each character on a new line.
+🎯 Example Output:
+P
+y
+t
+h
+o
+n
+'''
+
+my_str = "Python"
+
+for i in my_str:
+    print(i)
