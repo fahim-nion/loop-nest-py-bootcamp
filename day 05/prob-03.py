265b4b4e7ede6bd77b2103fb414ad5e1f7b162d3
Fahim Morshed Nion <fahimalhassan4@gmail.com>
2025-11-16 22:35:18 +0600

    exploring day 5

--- day 05/prob-03.py
+++ day 05/prob-03.py
@@ -1,1 +1,17 @@
-'''prob 3'''
+'''Problem 3: Even or Odd Checker
+Description:
+একটি ফাংশন লিখো  নামে, যেটি একটি সংখ্যা ইনপুট হিসেবে নিবে এবং বলবে এটি Even নাকি Odd।
+Example:
+check_even_odd(10) ➞ "Even"
+check_even_odd(7)  ➞ "Odd" '''
+
+def check_even_odd(val):
+  if val % 2==0:
+    print("Even")
+  else:
+    print("Odd")
+
+
+check_even_odd(10)
+check_even_odd(7)
+
