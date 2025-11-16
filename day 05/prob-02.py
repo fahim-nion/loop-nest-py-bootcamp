2e8213023b280f40597b82158e8e591629dbe072
Fahim Morshed Nion <fahimalhassan4@gmail.com>
2025-11-16 22:30:53 +0600

    exploring day 05

--- day 05/prob-02.py
+++ day 05/prob-02.py
@@ -1,1 +1,12 @@
-'''prob 2'''
+'''Problem 2: Area Calculator
+Description:
+একটি ফাংশন তৈরি করো  নামে, যেটি একটি আয়তক্ষেত্রের দৈর্ঘ্য ও প্রস্থ ইনপুট হিসেবে নিবে এবং এর ক্ষেত্রফল রিটার্ন করবে।
+Example:
+calculate_area(5, 3) ➞ 15'''
+
+def calculate_area(l,w):
+  area = l*w
+  return area
+
+
+print(calculate_area(3,5))
