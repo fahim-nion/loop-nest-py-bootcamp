5c8f7c401dae7c475138b23320342ed649d29618
fahim-nion <fahimalhassan4@gmail.com>
2025-11-18 22:24:44 +0600

    updated for final project

---
+++ practice project/project.py
@@ -0,0 +1,17 @@
+while True:
+    print("Welcome to Python Mini Calculator!")
+    
+    opt = int(input(
+        '''Choose an option:
+            1. Add
+            2. Subtract
+            3. Multiply
+            4. Divide
+            5. Modulus
+            6. Average of numbers
+            7. Maximum number
+            8. Exit
+        '''
+    ))
+    if opt == 1:
+        addi
