123f692a59a010ed3829c30cb4ad1f2692ad83e5
fahim-nion <fahimalhassan4@gmail.com>
2025-11-27 22:01:29 +0600

    updated

---
+++ day 06/prob-2.py
@@ -0,0 +1,14 @@
+'''
+### ✅ **Problem 2: Unique Fruits Organizer**
+
+You have the following list of fruits:
+
+```python
+fruits = ['apple', 'banana', 'apple', 'orange', 'banana', 'kiwi']
+
+```
+
+- Remove duplicates using a **set**
+- Convert it back to a list and sort alphabetically
+- Print the final clean fruit list
+'''
