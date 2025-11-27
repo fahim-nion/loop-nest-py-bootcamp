993f9665552a37d86a6014e39cd39f74cbe8059c
fahim-nion <fahimalhassan4@gmail.com>
2025-11-27 21:27:46 +0600

    updated

--- day 06/final-day.py
+++ day 06/final-day.py
@@ -1,1 +1,23 @@
-print("taju")
+'''
+
+### ✅ **Problem 1: Student Grade Calculator**
+
+Create a function called `calculate_grade()` that:
+
+- Takes a student’s name and marks of 3 subjects as input.
+- Calculates the average and determines the grade:
+    - A if avg ≥ 80
+    - B if avg ≥ 70
+    - C if avg ≥ 60
+    - F otherwise
+- Return a dictionary like this:
+
+```python
+{
+  'name': 'Rafi',
+  'average': 76.33,
+  'grade': 'B'
+}
+```
+
+'''
