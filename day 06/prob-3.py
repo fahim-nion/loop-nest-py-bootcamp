a517f514f5dbb3bc4a2bd6e6e1bf969e4e43e208
fahim-nion <fahimalhassan4@gmail.com>
2025-11-28 22:02:57 +0600

    final day

---
+++ day 06/prob-3.py
@@ -0,0 +1,17 @@
+'''
+### ✅ **Problem 3: User Profile Generator**
+
+Create a function called `create_profile()` that takes:
+
+- name (string), age (int), and skills (list of strings)
+- Returns a dictionary like this:
+
+```python
+{
+  'name': 'Anika',
+  'age': 21,
+  'skills': ['Python', 'HTML', 'CSS']
+}
+
+```
+'''
