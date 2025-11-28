5efb2c97d998800e84e72e66881ae6c1e60663f2
fahim-nion <fahimalhassan4@gmail.com>
2025-11-28 22:08:33 +0600

    final day

---
+++ day 06/prob-5.py
@@ -0,0 +1,17 @@
+'''
+### ✅ **Problem 5: Tuple Extractor**
+
+You have the following tuple:
+
+```python
+info = ('Rifat', 22, 'CSE', 'Khulna')
+
+```
+
+- Slice the tuple and extract only the name and subject (`'Rifat', 'CSE'`)
+- Convert that sliced part into a list and print it
+'''
+
+info = ('Rifat', 22, 'CSE', 'Khulna')
+
+print(info[::2])
